8239b52957b648cd5dfbb9bdeef3e4590b60f535
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2024-01-22 21:49:21 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 """ Main file to insert data to BQ"""
+import pandas as pd
+
+def main(filename):
+    """
+    Main funtion to read CSV file
+    """
+    df = pd.read_csv(file=filename)
+    return df
+
+if __name__ == "__main__":
+    FILENAME = '/table_schema/zwya_transactions.csv'
+    main(filename=FILENAME)
