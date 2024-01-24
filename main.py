db1de34669272af3d78605e2e01d777ee33de137
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2024-01-24 22:41:40 +0530

    Perform CURD and Insert operations

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 """ Main file to insert data to BQ"""
-import os
-
-from dotenv import load_dotenv
 from big_query import BigQueryOperations
 
 if __name__ == '__main__':
-    load_dotenv()
-
-    PROJECT_ID = os.environ.get('project_id')
-    DATASET_ID = os.environ.get('dataset_id')
-    CREDS_FILE = '/bq_service_account.json'
-    bq = BigQueryOperations(
-        project_id=PROJECT_ID,
-        dataset_id=DATASET_ID,
-        credentials_path=CREDS_FILE
-    )
-
     TABLE_ID = 'test_table'
-    TABLE_SCHEMA = '/table_schema/zwya_transactions.json'
+    TABLE_SCHEMA = 'table_schema/zwya_transactions.json'
+    TABLE_FILE_PATH = 'table_schema/zwya_transactions.xlsx'
 
-    is_exists = bq.table_exists(table_id=TABLE_ID)
-    if is_exists:
-        print(f'Table {TABLE_ID} already exists in BQ project.')
+    # Create BigQueryOperations instance
+    bq = BigQueryOperations()
+    # Create table
+    bq.create_table(
+        table_id=TABLE_ID,
+        schema=TABLE_SCHEMA
+    )
+    # Read data from specified file.
+    if '.csv' in TABLE_FILE_PATH:
+        print('reading CSV file')
+    elif '.xlsx' in TABLE_FILE_PATH:
+        print('reading XLSX file')
     else:
-        bq.create_table(table_id=TABLE_ID, schema=TABLE_SCHEMA)
+        print('We are working with integrating with different file format. : D')
+
+    # Insert data to BQ {TABLE_ID} table
+    
