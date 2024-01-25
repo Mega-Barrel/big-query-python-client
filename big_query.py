ecff92607172bbc75996189f556ab706e72eea9d
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2024-01-25 22:50:10 +0530

    added create_table code to create and insert data to table

--- big_query.py
+++ big_query.py
@@ -5,12 +5,15 @@ import os
 import json
 
 # Installed packages
+import pandas as pd
 from dotenv import load_dotenv
 from google.cloud import bigquery   #pylint: disable=E0401
 from google.oauth2 import service_account   #pylint: disable=E0401
 
 # Exceptions import
 from google.cloud.exceptions import NotFound    #pylint: disable=E0401
+from google.api_core.exceptions import BadRequest   #pylint: disable=E0401
+
 class BigQueryOperations:
     """Encapsulates operations for interacting with BigQuery tables."""
 
@@ -35,6 +38,8 @@ class BigQueryOperations:
             credentials=self.credentials,
             project=self._project_id
         )
+        # Create empty list
+        self._schema = []
 
     def table_exists(self, table_name: str):
         """Checks if a table exists in the dataset.
@@ -56,51 +61,77 @@ class BigQueryOperations:
             print(f'Table `{table_name}` does not exists.')
             return True
 
-    def schema_helper(self, schema_json):
+    def schema_helper(self, schema: dict) -> None:
         """Parses a JSON string representing a schema and returns a list of BigQuery SchemaField objects.
         Supports additional attributes like "mode" and "description" for columns.
 
         Args:
-            schema_json (str): A JSON string representing the schema.
+            schema (dict): A JSON string representing the schema.
 
         Returns:
             list: A list of BigQuery SchemaField objects.
         """
+        with open(schema, encoding='utf-8') as json_data:
+            data = json.load(json_data)
+            for name, field_info in data.items():
+                field_type = field_info.get("type")
+                description = field_info.get("description")
+                # Append data to schema list
+                self._schema.append(
+                    bigquery.SchemaField(
+                        name=name,
+                        field_type=field_type,
+                        description=description
+                    )
+                )
 
-    def create_table(self, table_id: str, schema: json):
+    def create_table(self, df: pd.DataFrame, table_name: str, schema=None):
         """Creates a new table in the specified dataset.
 
         Args:
-            table_id (str): The name of the table to create.
-            schema (list): A list of BigQuery schema fields for the table.
+            df (DataFrame): Pandas DataFrame.
+            table_name (str): The name of the table to create.
+            schema [list] (Optional): A list of BigQuery schema fields for the table.
         """
+        # define table_id
+        table_id = f'{self._project_id}.{self._dataset_id}.{table_name}'
         # Check if table exists, if not create table with defined schema
-        if self.table_exists(table_id):
-            # table = bigquery.Table(table_id, schema=schema)
-            # table = self._client.create_table(table)  # Make an API request.
-            print(f"Table created with following name: {table_id}.")
+        if self.table_exists(table_name):
+            if schema is None:
+                print(f"Creating table: {table_name}, with schema defined in pandas dataframe.")
+                # Test
+                job = self._client.load_table_from_dataframe(
+                    df, table_id
+                )
+                try:
+                    job.result()
+                    print(f'Table: {table_name} created and data inserted successfully.')
+                except BadRequest as error:
+                    for error in job.errors:
+                        print(f'ERROR: {error["message"]}')
+            else:
+                print('Calling Schema_helper function.')
+                self.schema_helper(schema)
+                print(f'Creating table: {table_name}, with custom schema specified.')
+                # Table with custom schema
+                job_config = bigquery.LoadJobConfig(schema=self._schema)
+                job = self._client.load_table_from_dataframe(
+                    df, table_id, job_config=job_config
+                )
+                try:
+                    job.result()
+                    print(f'Table: {table_name} created and data inserted successfully.')
+                except BadRequest as error:
+                    for error in job.errors:
+                        print(f'ERROR: {error["message"]}')
 
-    def auto_detect_schema(self):
-        """ Auto detects schema from excel/csv file.
-        
-        Args:
-            file_name (str): Name of the file
-        """
-
-    def insert_data(self):
-        """Inserts data into a table.
-
-        Args:
-            table_id (str): The name of the table to insert data into.
-            data (list or str): The data to insert, either as a list of tuples
-                or a JSON string.
-            json_format (bool, optional): True if the data is in JSON format,
-                False if it's a list of tuples. Defaults to False.
-        """
-
-    def delete_table(self):
+    def delete_table(self, table_name: str) -> None:
         """Deletes a table from the dataset.
 
         Args:
-            table_id (str): The name of the table to delete.
+            table_name (str): The name of the table to delete.
         """
+        # define table_id
+        table_id = f'{self._project_id}.{self._dataset_id}.{table_name}'
+        self._client.delete_table(table_id, not_found_ok=True)  # Make an API request.
+        print(f"Deleted table '{table_name}'.")
