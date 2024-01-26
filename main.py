1082a653596ec7963a82399ec6b1cf8ad7726a4f
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2024-01-26 16:00:07 +0530

    added new datasets

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import pandas as pd
 from big_query import BigQueryOperations
 
 if __name__ == '__main__':
-    TABLE_ID = 'zwya_transactions'
-    TABLE_SCHEMA = 'table_schema/zwya_transactions.json'
-    TABLE_FILE_PATH = 'table_schema/zwya_transactions.csv'
+    TABLE_ID = 'games2win_assignment_funnel_analysis'
+    # TABLE_SCHEMA = 'table_schema/zwya_transactions.json'
+    TABLE_FILE_PATH = 'table_schema/games2win_assignment_funnel_analysis.csv'
 
     # Create BigQueryOperations instance
     bq = BigQueryOperations()
@@ -15,9 +15,15 @@ if __name__ == '__main__':
         TABLE_FILE_PATH
     )
 
+    # Read data from file (with custom schema).
+    # bq.create_table(
+    #     df=data_frame,       # Pandas DataFrame
+    #     table_name=TABLE_ID, # Table name
+    #     schema=TABLE_SCHEMA  # Table schema
+    # )
+
     # Read data from file.
     bq.create_table(
         df=data_frame,       # Pandas DataFrame
-        table_name=TABLE_ID, # Table name
-        schema=TABLE_SCHEMA  # Table schema
+        table_name=TABLE_ID  # Table name
     )
